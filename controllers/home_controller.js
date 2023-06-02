ff8b9ae5566d059b800dc2ffba6cfa643aedbfb3
shashank <shashankpunna21@gmail.com>
2023-06-02 11:45:50 +0530

    setup layouts and partials

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Server is up for Codial!</h1>')
+    return res.render("home",{
+        title:"Home"
+    });
 }
 
 module.exports.feed = function(req,res){
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>Users Profile</h1>')
+    return res.render("user_profile",{
+        title:"Profile"
+    })
 }
 
 module.exports.edit=function(req,res){
