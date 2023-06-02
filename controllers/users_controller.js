385ae369edd45d335fe106c313d8eb02df1af286
shashank <shashankpunna21@gmail.com>
2023-06-02 14:28:36 +0530

    setup user schema and create signup and signin pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,3 +7,23 @@ module.exports.profile=function(req,res){
 module.exports.edit=function(req,res){
     return res.end('<h1>Edit Profile</h1>')
 }
+
+module.exports.signIn = function(req,res){
+    return res.render("user_sign_in",{
+        title:"Sign In"
+    })
+}
+
+module.exports.signUp=function(req,res){
+    return res.render("user_sign_up",{
+        title:"Sign Up"
+    })
+}
+
+module.exports.create=function(req,res){
+  //todo
+}
+
+module.exports.createSession=function(req,res){
+  //todo
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ const usersController=require('../controllers/users_controller');
 router.get('/profile',usersController.profile)
 router.get('/edit',usersController.edit)
 
+router.get('/sign-in',usersController.signIn)
+router.get('/sign-up',usersController.signUp)
+
+router.post('/create',usersController.create)
+router.post('/create-session',usersController.createSession)
+
 module.exports = router;
