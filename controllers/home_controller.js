195da98675278090545e147f556f5eb35cba4566
shashank <shashankpunna21@gmail.com>
2023-06-03 13:53:38 +0530

    Create post to database and show on homescreen also prepopulate the user data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,25 @@
+const Post =require('../models/post');
 module.exports.home = function(req,res){
 
-    console.log(req.cookies)
-    res.cookie('user',45);
-    return res.render("home",{
-        title:"Home"
-    });
+    //console.log(req.cookies)
+    //res.cookie('user',45);
+
+   /* Post.find({}).then((posts)=>{
+        return res.render("home",{
+            title:"Home",
+            posts:posts
+        });
+    }).catch((error)=>{
+        console.log('Error in fetching posts ---> home controller',error)
+    })*/
+
+    Post.find({}).populate('user').then((posts)=>{
+        return res.render("home",{
+            title:"Codial | Home",
+            posts:posts
+        })
+    }).catch((error)=>{console.log('Error in fetching posts --> Home Controller',error)})
+    
 }
 
 module.exports.feed = function(req,res){
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,17 @@
+const Post=require("../models/post")
 module.exports.create=function(req,res){
-    return res.end("<h1> Create Posts Page</h1>")
+
+    Post.create({
+        content:req.body.content,
+        user:req.user._id
+    }).then((user)=>{
+        //console.log(user)
+        return res.redirect('back')
+    }).catch((error)=>{
+        if(error){
+            console.log("Error in creating a post");
+            return;
+        }
+    })
+    
 }
